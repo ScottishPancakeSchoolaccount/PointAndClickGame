0304b40196f465fc089811c21a210bebefc98705
ScottishPancakeSchoolaccount <144125195+ScottishPancakeSchoolaccount@users.noreply.github.com>
2024-03-04 10:18:56 +0100

    teehee pointyclickhy

--- js/main.js
+++ js/main.js
@@ -1,97 +1,108 @@
-// A document is what we call out HTML page
 document.getElementById("mainTitle").innerText = "Point and Click adventure game";
+// Game State
+let gameState = {
+    "inventory": [],
+    "coinPickedUp": false,
+    "keyPickedUp": false
+}
+//localStorage.removeItem("gameState");
+if (Storage) {
+    if (localStorage.gameState) {
+        //uses localStorage gameState string and convert it to an object. then store it into gameState
+        gameState = JSON.parse(localStorage.gameState);
+    } else { localStorage.setItem("gameState", JSON.stringify(gameState)) } //convert local object variable to a string. then store it into localStorage
+}
 
-//const prevents it from breaking the game window reference
+//Game window reference
 const gameWindow = document.getElementById("gameWindow");
-
+const inventoryList = document.getElementById("inventoryList");
 const sec = 1000;
 
-//Main character lines
+//Main Character
 const mainCharacter = document.getElementById("hero");
-const offCharacter = 16;
+const offsetCharacter = 20;
 
-//Avatar
-const counterAvatar = document.getElementById("counterAvatar")
-
-//audio
-const heroAudio = document.getElementById("heroSpeech");
-const chickenAudio = document.getElementById("counterSpeech");
-
-
-//Inventory
-let Inventory = [];
-const inventoryList = document.getElementById("inventoryList");
-
-//Speech bubles
+//speech bubbles
 const heroSpeech = document.getElementById("heroSpeech");
-const counterSpeech = document.getElementById("counterSpeech");
+const counsterSpeech = document.getElementById("counterSpeech");
+//audio for dialog
+const heroAudio = document.getElementById("heroAudio");
+const counterAudio = document.getElementById("counterAudio");
 
-//Trees
+//avatar
+const counterAvatar = document.getElementById("counterAvatar");
+
+//Objects
 const tree1 = document.getElementById("squareTree");
 
-//check if dialog is active
-let checkDialogue = false;
+if (gameState.keyPickedUp) {
+    document.getElementById("key").remove();
+}
 
-//First java script function
-gameWindow.onclick = function (e) { //e for event
+updateInventory(gameState.inventory, inventoryList);
+
+
+gameWindow.onclick = function (e) {
     var rect = gameWindow.getBoundingClientRect();
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    //Character movement
-    //FIX: Character doesn't animate on first click.
-    //FIX: Character goes out of bounds when you click on the character itself.
     //TODO: calc offset based on character size
+    //TODO: making dialog functionality
 
-    //This means that if you click anything else but the hero cube itself, it moves and prevents it going out of the canvas.
-    if (e.target.id !== "hero" && counterSpeech.style.opacity == 0 && heroSpeech.style.opacity == 0) {
-        mainCharacter.style.left = x - offCharacter + "px";
-        mainCharacter.style.top = y - offCharacter + "px";
-    }
-
-    //This does the same as the code below, but nicer 
-    switch (e.target.id) {
-        case "squareTree":
-            tree1.style.opacity = 0.5;
-            break;
-        case "key":
-            console.log("Oh sweet! I got a key!");
-            document.getElementById("key").remove();
-            changeInventory('key', "add");
-            break;
-        case "mushroom":
-            console.log("Oh sweet! This mushroom is looking mighty tasty!");
-            changeInventory("mushroom", "add");
-            document.getElementById("mushroom").remove();
-            break;
-        case "chicken":
-            console.log("bok bok");
-            console.log("opbok");
-        default:
-            tree1.style.opacity = 1;
-            break;
-        case "doorSmallShak":
-            if (checkItem("key")) {
-                console.log("Door has been opened");
-            } else if (checkItem("mushroom")) {
-                changeInventory("mushroom", "remove")
-                console.log("Shit, I lost my yummy mushroom!")
-            } else {
-                console.log("Shit, I need a key because its locked.")
-            }
-
-        case "chicken":
-            showMessage(heroSpeech, "Hey there Chickems! How are you?", heroAudio);
-            setTimeout(function () { counterAvatar.style.opacity = 1; })
-            setTimeout(showMessage, 4 * sec, counterSpeech, "Bok bok..", chickenAudio);
-            setTimeout(showMessage, 8 * sec, heroSpeech, "Teehee, silly chicken :3", heroAudio);
-            setTimeout(showMessage, 12 * sec, counterSpeech, "You aren in grave danger.", chickenAudio);
-            setTimeout(showMessage, 14 * sec, heroSpeech, "Wha-What the fuck!?", heroAudio);
-            setTimeout(showMessage, 18 * sec, heroSpeech, "You can talk!?");
-            setTimeout(function () { counterAvatar.style.opacity = 0; });
-
+    if (counterSpeech.style.opacity == 0 && heroSpeech.style.opacity == 0) {
+        if (e.target.id !== "heroImage") {
+            mainCharacter.style.left = x - offsetCharacter + "px";
+            mainCharacter.style.top = y - offsetCharacter + "px";
+        }
+        switch (e.target.id) {
+            case "key":
+                showMessage(heroSpeech, "Sweet! I  found a key, maybe it's for the wizard door.", heroAudio);
+                document.getElementById("key").remove();
+                changeInventory('key', "add");
+                gameState.keyPickedUp = true;
+                saveBiscuits(gameState);
+                break;
+            case "well":
+                if (gameState.coinPickedUp == false) {
+                    changeInventory("coin", "add");
+                    gameState.coinPickedUp = true;
+                } else {
+                    console.log("There are no more coins in this well!");
+                }
+                break;
+            case "doorWizardHut":
+                if (checkItem("key")) {
+                    showMessage(heroSpeech, "I opened the door. Yeah!", heroAudio);
+                    //console.log("I opened the door. Yeah!");
+                } else if (checkItem("coin")) {
+                    changeInventory("coin", "remove");
+                    showMessage(heroSpeech, "Oh no I lost the coin! I might've needed that for something..", heroAudio);
+                    //console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
+                } else {
+                    showMessage(heroSpeech, "Fuck. It's locked, of course.", heroAudio);
+                    //console.log("Fuck this door is locked and I don't have a key. boohoo :(");
+                }
+                break;
+            case "statue":
+                showMessage(heroSpeech, "Teehee, hello chicken! ^^", heroAudio);
+                setTimeout(function () { counterAvatar.style.opacity = 1; }, 4 * sec);
+                setTimeout(showMessage, 4.1 * sec, counsterSpeech, "What do you want, knobhead?", counterAudio);
+                setTimeout(showMessage, 8.1 * sec, heroSpeech, "You can talk!?", heroAudio);
+                setTimeout(showMessage, 12.1 * sec, counsterSpeech, "Of course I can, you prick. You are a talking cat.", counterAudio);
+                setTimeout(showMessage, 16.1 * sec, heroSpeech, "Oh, right. Well could you tell me where I can find the key for the wizard hut?", heroAudio);
+                setTimeout(showMessage, 20.1 * sec, counsterSpeech, "Tell ya what, if you could pay me first, then i'll give you a clue.", counterAudio);
+                // setTimeout(showMessage, 24.1 * sec, counsterSpeech, "Gowan! Get a move on, you prick!", counterAudio);
+                setTimeout(showMessage, 24.1 * sec, heroSpeech, "Knob.", heroAudio);
+                setTimeout(function () { counterAvatar.style.opacity = 0; }, 40 * sec); //This is the duration of the chicken sprite
+                //console.log("hey you.. wanna know where the key is? It's by the graves.");
+                break;
+            default:
+                break;
+        }
     }
 }
+
 /**
  * Add or remove item in inventory
  * @param {string} itemName 
@@ -99,85 +110,72 @@ gameWindow.onclick = function (e) { //e for event
  */
 function changeInventory(itemName, action) {
     if (itemName == null || action == null) {
-        console.error("Ruh Roh, it went wrong in changeInventory()");
+        console.error("Wrong parameters given to changeInventory()");
         return;
     }
 
     switch (action) {
         case 'add':
-            Inventory.push(itemName);
+            gameState.inventory.push(itemName);
             break;
         case 'remove':
-            Inventory = Inventory.filter(function (newInventory) {
+            gameState.inventory = gameState.inventory.filter(function (newInventory) {
                 return newInventory !== itemName;
             });
             document.getElementById("inv-" + itemName).remove();
             break;
-    }
-    updateInventory(Inventory, inventoryList);
 
+    }
+    updateInventory(gameState.inventory, inventoryList);
 }
 
 /**
- * need a name for displaying items and html id name, checks if the value exists adds value to the array
- * if not then it adds value to the array and use function
+ * This returns string value if it exist within the array
  * @param {string} itemName 
- * @param {string} itemId 
+ * @returns 
  */
-function showItem(itemName, itemId) {
-    console.log('You\'ve found a ' + itemName + '!');
-    const keyElement = document.createElement("li");
-    keyElement.id = 'inv-' + itemId;
-    keyElement.innerText = itemName;
-    inventoryList.appendChild(keyElement);
+function checkItem(itemName) {
+    return gameState.inventory.includes(itemName);
 }
 
-function removeItem(itemName, itemId) {
-    //Removes item in array
-    Inventory = Inventory.filter(function (newInventory) {
-        return newInventory !== itemName;
-    });
-    document.getElementById(itemId).remove();
-}
-
-function updateInventory(Inventory, inventoryList) {
+function updateInventory(inventory, inventoryList) {
     inventoryList.innerHTML = '';
-    Inventory.forEach(function (item) {
+    inventory.forEach(function (item) {
         const inventoryItem = document.createElement("li");
         inventoryItem.id = 'inv-' + item;
         inventoryItem.innerText = item;
-        inventoryList.appendChild(inventoryItem)
-
-    });
+        inventoryList.appendChild(inventoryItem);
+    })
 }
 
 /**
- * 
+ * It will show dialog and trigger sound.
  * @param {getElementById} targetBubble 
- * @param {string} message 
+ * @param {string} message
+ * @param {getElementById} targetSound 
  */
 function showMessage(targetBubble, message, targetSound) {
     targetSound.currentTime = 0;
     targetSound.play();
     targetBubble.innerText = message;
     targetBubble.style.opacity = 1;
-    setTimeout(hideMessage, 4 * sec, targetBubble);
+    setTimeout(hideMessage, 4 * sec, targetBubble, targetSound);
 }
+
 /**
- * 
- * @param {*} targetBubble 
- * @param {*} targetSound 
+ * Hides message and pauze the audio
+ * @param {getElementById} targetBubble 
+ * @param {getElementById} targetSound 
  */
 function hideMessage(targetBubble, targetSound) {
     targetSound.pause();
     targetBubble.innerText = "...";
     targetBubble.style.opacity = 0;
-    checkDialogue = false;
-
 }
-
-//showMessage(heroSpeech, "Wagwan lad.")
-//showMessage(counterSpeech, "Wagwan.")
-
-
-//runGame();
+/**
+ * 
+ * @param {*} gameState 
+ */
+function saveBiscuits(gameState) {
+    localStorage.gameState = JSON.stringify(gameState);
+}
